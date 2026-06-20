40d230c486ffba985a19b4ca79b33574a5da33c8
asher2red <asher2red@gmail.com>
2026-06-20 22:54:51 +0900

    feat: spawn 컨트롤 hud 추가

--- Assets/Scripts/Spawn/Spawner.cs
+++ Assets/Scripts/Spawn/Spawner.cs
@@ -1,15 +1,67 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
+    [SerializeField] private float spawnRate = 100f;
+    public int Count => objects.Count;
+    private readonly List<GameObject> objects = new();
+    private bool autoSpawn;
 
     public void Spawn()
     {
-        Vector3 position = new Vector3(Random.Range(-5f, 5f),
+        Vector3 position = new Vector3(
+            Random.Range(-5f, 5f),
             Random.Range(-3f, 3f),
             0f);
         
-        Instantiate(prefab, position, Quaternion.identity);
+        var obj = Instantiate(prefab, position, Quaternion.identity);
+
+        objects.Add(obj);
+    }
+
+    public void SpawnMany(int count)
+    {
+        for (int i = 0; i<count; i++)
+        {
+            Spawn();
+        }
+    }
+
+    public void Clear()
+    {
+        foreach (var obj in objects)
+        {
+            if (obj != null)
+            {
+                Destroy(obj);
+            }
+        }
+
+        objects.Clear();
+    }
+
+    public void setAutoSpawn(bool value)
+    {
+        autoSpawn = value;
+    }
+
+    private void Update()
+    {
+        if (!autoSpawn)
+        return;
+
+        float count = spawnRate * Time.deltaTime;
+
+        for (int i = 0; i < count; i++)
+        {
+            Spawn();
+        }
+    }
+
+    public void SetSpawnRate(float value)
+    {
+        spawnRate = value;
     }
 }
---
+++ Assets/Scripts/UI/StatsUI.cs
@@ -0,0 +1,26 @@
+using TMPro;
+using UnityEngine;
+
+public class StatsUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI fpsText;
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private Spawner spawner;
+
+    private float timer;
+
+    private void Update()
+    {
+        timer += Time.unscaledDeltaTime;
+
+        if (timer < 0.5f)
+            return;
+
+        timer = 0f;
+
+        float fps = 1f / Time.unscaledDeltaTime;
+
+        fpsText.text = $"FPS: {fps:F0}";
+        countText.text = $"Objects: {spawner.Count}";
+    }
+}
